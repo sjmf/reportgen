c5b46fc09005260e4fe918e576074987f76c6896
Sam Mitchell Finnigan <s.j.finnigan@ncl.ac.uk>
2016-04-26 23:35:48 +0100

    Fix file uploads

--- flask-uploads.py
+++ flask-uploads.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # coding: utf-8
 import io, os, errno, re, json, tempfile
-from flask import Flask, request, redirect, url_for, send_from_directory, render_template, session
+from flask import Flask, request, Response, redirect, url_for, send_from_directory, render_template, session
 from flask.ext.session import Session
 from werkzeug import secure_filename
 
@@ -18,11 +18,10 @@ Session(app)
 
 flask_options = {
     'host':'0.0.0.0',
-    'threaded':True
+#    'threaded':True
 }
 
 # Make the upload directory
-# Replace this with redis in future
 def mkdir_p(path):
     try:
         os.makedirs(path)
@@ -37,9 +36,15 @@ def escape_filename(filename):
 def list_uploaded_files():
     print(session)
     if 'files' in session:
-        return json.dumps([(fil['original_name'], fil['size']) for fil in session['files']])
+        return Response(
+                json.dumps([
+                    (fil['original_name'], fil['size']) 
+                        for fil in session['files']
+                ]),
+                mimetype='application/json')
     else:
-        return '[]'
+        return Response( '[]', 
+                mimetype='application/json')
 
 @app.route('/files/<path:path>', methods=['DELETE'])
 def delete_file(path):
@@ -51,49 +56,37 @@ def clear_files():
     session['files'] = []
     return 'Files cleared', 200
 
-@app.route('/upload', methods=['POST'])
-def upload_file():
-    return 'OK', 200
-    if request.method == 'POST' or request.method == 'PUT':
-        fil = request.files['file']
-        if fil and allowed_file(fil.filename):
-            filename = secure_filename(fil.filename)
-            fil.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return  'OK',200
+@app.route("/upload", methods=['PUT'])
+def upload():
+    fil = request.files['file']
+    print('\n'.join(dir(fil)))
 
-    return 'NO', 401
+    if 'files' not in session:
+        session['files'] = []
 
-#@app.route("/upload", methods=['PUT'])
-#def upload():
-#    fil = request.files['file']
-#    print('\t'.join(fil))
-#
-#    if 'files' not in session:
-#        session['files'] = []
-#
-#    original_name = escape_filename(fil.filename)
-#    temporary_name = next(tempfile._get_candidate_names())
-#    file_extension = original_name.rsplit('.', 1)[1] if '.' in original_name else ''
-#
-#    if fil and file_extension in ALLOWED_EXTENSIONS:
-#        # Put it on the disk:
-#        fil.save(os.path.join(app.config['UPLOAD_FOLDER'], temporary_name))
-#
-#        # Put it in the session (redis- experimental)
-#        #output = io.BytesIO()
-#        #fil.save(output)
-#
-#        session['files'].append({
-#            'original_name'   : original_name,
-#            'temporary_name'  : temporary_name,
-#            'file_extension'  : file_extension,
-#            'size'  : fil.content_length,
-#            #'bytes' : output,
-#        })
-#        print("{} files uploaded".format(len(session['files'])))
-#
-#        return "File Received: {}".format(original_name), 200#1 #redirect('job')
-#    return "File type '{}' is not allowed.".format(original_name.split('.')[1]), 400
+    original_name = fil.filename#escape_filename(fil.filename)
+    temporary_name = next(tempfile._get_candidate_names())
+    file_extension = original_name.rsplit('.', 1)[1] if '.' in original_name else ''
+
+    if fil and file_extension in ALLOWED_EXTENSIONS:
+        # Put it on the disk:
+        fil.save(os.path.join(app.config['UPLOAD_FOLDER'], temporary_name))
+
+        # Put it in the session (redis- experimental)
+        #output = io.BytesIO()
+        #fil.save(output)
+
+        session['files'].append({
+            'original_name'   : original_name,
+            'temporary_name'  : temporary_name,
+            'file_extension'  : file_extension,
+            'size'  : fil.content_length,
+            #'bytes' : output,
+        })
+        print("{} files uploaded".format(len(session['files'])))
+
+        return "File Received: {}".format(original_name), 200#1 #redirect('job')
+    return "File type '{}' is not allowed.".format(original_name.split('.')[1]), 401
 
 
 @app.route('/', methods=['GET', 'POST'])
