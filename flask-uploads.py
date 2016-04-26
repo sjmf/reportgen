719d45a2a56f27a071ed93b478a894db1cf5e377
Sam Mitchell Finnigan <s.j.finnigan@ncl.ac.uk>
2016-04-26 16:44:21 +0100

    test

--- flask-uploads.py
+++ flask-uploads.py
@@ -16,6 +16,11 @@ SESSION_TYPE = 'redis'
 app.config.from_object(__name__)
 Session(app)
 
+flask_options = {
+    'host':'0.0.0.0',
+    'threaded':True
+}
+
 # Make the upload directory
 # Replace this with redis in future
 def mkdir_p(path):
@@ -31,7 +36,10 @@ def escape_filename(filename):
 @app.route("/files", methods=['GET', 'POST'])
 def list_uploaded_files():
     print(session)
-    return json.dumps([(fil['original_name'], fil['size']) for fil in session['files']])
+    if 'files' in session:
+        return json.dumps([(fil['original_name'], fil['size']) for fil in session['files']])
+    else:
+        return '[]'
 
 @app.route('/files/<path:path>', methods=['DELETE'])
 def delete_file(path):
@@ -43,37 +51,49 @@ def clear_files():
     session['files'] = []
     return 'Files cleared', 200
 
-@app.route("/upload", methods=['PUT'])
-def upload():
-    fil = request.files['file']
-    print('\t'.join(fil))
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    return 'OK', 200
+    if request.method == 'POST' or request.method == 'PUT':
+        fil = request.files['file']
+        if fil and allowed_file(fil.filename):
+            filename = secure_filename(fil.filename)
+            fil.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return  'OK',200
 
-    if 'files' not in session.keys():
-        session['files'] = []
+    return 'NO', 401
 
-    original_name = escape_filename(fil.filename)
-    temporary_name = next(tempfile._get_candidate_names())
-    file_extension = original_name.rsplit('.', 1)[1] if '.' in original_name else ''
-
-    if fil and file_extension in ALLOWED_EXTENSIONS:
-        # Put it on the disk:
-        fil.save(os.path.join(app.config['UPLOAD_FOLDER'], temporary_name))
-
-        # Put it in the session (redis- experimental)
-        #output = io.BytesIO()
-        #fil.save(output)
-
-        session['files'].append({
-            'original_name'   : original_name,
-            'temporary_name'  : temporary_name,
-            'file_extension'  : file_extension,
-            'size'  : fil.content_length,
-            #'bytes' : output,
-        })
-        print("{} files uploaded".format(len(session['files'])))
-
-        return "File Received{}".format(original_name), 201 #redirect('job')
-    return "File type '{}' is not allowed.".format(original_name.split('.')[1]), 400
+#@app.route("/upload", methods=['PUT'])
+#def upload():
+#    fil = request.files['file']
+#    print('\t'.join(fil))
+#
+#    if 'files' not in session:
+#        session['files'] = []
+#
+#    original_name = escape_filename(fil.filename)
+#    temporary_name = next(tempfile._get_candidate_names())
+#    file_extension = original_name.rsplit('.', 1)[1] if '.' in original_name else ''
+#
+#    if fil and file_extension in ALLOWED_EXTENSIONS:
+#        # Put it on the disk:
+#        fil.save(os.path.join(app.config['UPLOAD_FOLDER'], temporary_name))
+#
+#        # Put it in the session (redis- experimental)
+#        #output = io.BytesIO()
+#        #fil.save(output)
+#
+#        session['files'].append({
+#            'original_name'   : original_name,
+#            'temporary_name'  : temporary_name,
+#            'file_extension'  : file_extension,
+#            'size'  : fil.content_length,
+#            #'bytes' : output,
+#        })
+#        print("{} files uploaded".format(len(session['files'])))
+#
+#        return "File Received: {}".format(original_name), 200#1 #redirect('job')
+#    return "File type '{}' is not allowed.".format(original_name.split('.')[1]), 400
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -98,4 +118,5 @@ def send_font(path):
 
 if __name__ == "__main__":
     mkdir_p(app.config['UPLOAD_FOLDER'])
-    app.run()
+    app.run(**flask_options)
+
