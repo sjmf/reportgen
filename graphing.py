6546915dca27c18d3f6e10fc5cfb88a8cf14cf6a
Sam Mitchell Finnigan <s.j.finnigan@ncl.ac.uk>
2017-11-30 17:57:31 +0000

    Move imports to allow changing the matplotlib backend

--- graphing.py
+++ graphing.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python3
 # coding: utf-8
-import mpl_toolkits.axisartist
-import matplotlib as mpl
-import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import base64
@@ -16,6 +13,11 @@ from pandas.tseries.offsets import MonthEnd, MonthBegin, Week
 from datetime import datetime, timedelta
 from io import BytesIO
 from os import makedirs
+
+import matplotlib as mpl
+# mpl.use('agg')  # Must do this before pyplot import
+import matplotlib.pyplot as plt
+import mpl_toolkits.axisartist
 
 log = logging.getLogger(__name__)
 
